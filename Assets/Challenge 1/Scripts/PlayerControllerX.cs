28e52f91f75691175875d50ec4730e4fb35dcb22
HereisTaeHyun <kth882500@gmail.com>
2025-03-11 15:47:27 +0900

    UI 추가

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControllerX : MonoBehaviour
 {
@@ -8,16 +11,27 @@ public class PlayerControllerX : MonoBehaviour
     public float rotationSpeed;
     public float verticalInput;
     public GameObject Propeller;
+    public TextMeshProUGUI gameoverText;
+    public TextMeshProUGUI flightTimeText;
+    private bool isGameover;
+    private float flightTime;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        isGameover = false;
+        rb = GetComponent<Rigidbody>();
+        flightTime = 0;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(isGameover == true)
+        {
+            return;
+        }
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
@@ -27,6 +41,21 @@ public class PlayerControllerX : MonoBehaviour
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right, verticalInput * rotationSpeed * Time.deltaTime);
 
-        Propeller.transform.Rotate(new Vector3(0, 0, 100) * Time.deltaTime);
+        Propeller.transform.Rotate(new Vector3(0, 0, 1000) * Time.deltaTime);
+
+        flightTime += Time.deltaTime;
+
+        flightTimeText.text = $"{(int)flightTime}";
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Wall"))
+        {
+            rb.velocity = Vector3.zero;
+            gameoverText.text = "Game Over ! ";
+            isGameover = true;
+            Debug.Log("GameOver");
+        }
     }
 }
