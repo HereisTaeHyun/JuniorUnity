24ce44a43eedb745d207af78a34e7b2fec54e223
HereisTaeHyun <kth882500@gmail.com>
2025-03-26 15:36:15 +0900

    0326

--- Assets/2. Scripts/PlayerCtrl.cs
+++ Assets/2. Scripts/PlayerCtrl.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float turmSpeed = 20.0f;
+    private Rigidbody rb;
+    [SerializeField] GameObject centerOfMass;
+    [SerializeField] private float horsePower = 20.0f;
+    [SerializeField] private float turmSpeed = 20.0f;
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] float speed;
+    [SerializeField] TextMeshProUGUI rpmText;
+    [SerializeField] float rpm;
     private float h;
     private float v;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centerOfMass.transform.position;
     }
 
     // Update is called once per frame
@@ -20,8 +28,13 @@ public class PlayerCtrl : MonoBehaviour
         v = Input.GetAxis("Vertical");
         h = Input.GetAxis("Horizontal");
         // 장비 움직이기
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * v);
-        // transform.Rotate(Vector3.up, h * turmSpeed * Time.deltaTime);
+        speed = Mathf.Round(rb.velocity.magnitude * 2.237f);
+        speedometerText.SetText($"Speed : {speed}");
+
+        rpm = Mathf.Round((speed % 30) * 40);
+        rpmText.SetText($"RPM : {rpm}");
+
+        rb.AddRelativeForce(Vector3.forward * horsePower * v);
 
         if(v != 0)
         {
